f4699d9693129ce68c8afb1246bd4381f1e9f029
Dreykson Oliveira Bessa <dreyksonbessa16@hotmail.com>
2022-04-12 20:41:53 -0300

    update variable

--- src/api/services/telefoneService.js
+++ src/api/services/telefoneService.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const client = require('twilio')(process.env.ACCOUNT_SID_TWILIO_DEV, process.env.AUTH_TOKEN_TWILIO_DEV);
+const client = require('twilio')(process.env.ACCOUNT_SID_TWILIO, process.env.AUTH_TOKEN_TWILIO);
 const STATUS_CODE = require('../../constantes/status_code.json');
 const MESSAGES = require('../../constantes/messages.json');
 const { createMessageError } = require('../../utils/utils');
@@ -9,7 +9,7 @@ module.exports = {
         try {
             await client.messages.create({
                 body: "Bem vindo a FENAPFF. O seu código de verificação é: " + codigo,
-                from: process.env.PHONE_NUMBER_DEV,
+                from: process.env.PHONE_NUMBER,
                 to: numberSend
             });
         } catch (error) {
